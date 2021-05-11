b308baaa11a955714a53d6b697dbfd72eaaea373
amiraayadi01 <77847542+amiraayadi01@users.noreply.github.com>
2021-05-11 04:30:32 +0100

    Update ArrayQueue.java

--- ie.ucd.datastructures/src/ArrayQueue.java
+++ ie.ucd.datastructures/src/ArrayQueue.java
@@ -1,77 +1,6 @@
 public class ArrayQueue<E> implements Queue<E> {
-
-	private static final int CAPACITY = 10; // default capacity if user doesn't provide one during queue creation
-	// instance variables
-	private int size = 0;
-	private int firstElement = 0;
-	private E[] data;
-
-	// constructors
-	public ArrayQueue() {
-		this(CAPACITY);
-	} // constructs queue with default capacity if no capacity passed to the object
-
-	@SuppressWarnings("unchecked")
-	public ArrayQueue(int capacity) {
-		data = (E[]) new Object[capacity];
-	}
-
-	// methods
-	@Override
-	public int size() {
-		return size;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return size == 0;
-	}
-
-	@Override
-	public void enqueue(E e) throws IllegalStateException {
-		if (size == data.length) {
-			throw new IllegalStateException("Queue is full");
-		}
-		int avail = (firstElement + size) % data.length;
-		data[avail] = e;
-		size++;
-	}
-
-	//returns first element
-	@Override
-	public E first() {
-		if (isEmpty()) {
-			return null;
-		}
-		return data[firstElement];
-	}
-
-	@Override
-	public E dequeue() {
-		if (isEmpty()) {
-			return null;
-		}
-		E answer = data[firstElement];
-		data[firstElement] = null;
-		firstElement = (firstElement + 1) % data.length;
-		size--;
-		return answer;
-	}
 	
-	@Override
-	public String toString() {
-		if (isEmpty()) {
-			return "[]";
-		}
-		String newString = "[" + data[0];
-
-		for (int i = 1; i < size(); i++) {
-			newString = newString + ", " + data[i];
-		}
-
-		return newString + "]";
-	}
-
+	
 	public static void main(String[] args) {
 		// Create a queue of default capacity (10)
 		ArrayQueue<Integer> q = new ArrayQueue<>();
@@ -97,6 +26,53 @@ public class ArrayQueue<E> implements Queue<E> {
 
 		System.out.println("\nMore detailed testing in junit file.\n***PASSED ALL TESTS***");
 
+	}
+
+
+	private static final int CAPACITY = 10; 
+	// instance variables
+	private int size = 0;
+	private int firstElement = 0;
+	private E[] data;
+	
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public void enqueue(E e) throws IllegalStateException {
+		if (size == data.length) {
+			throw new IllegalStateException("Queue is reached full capacity");
+		}
+		int avail = (firstElement + size) % data.length;
+		data[avail] = e;
+		size++;
+	}
+	
+	@Override
+	public E first() {
+		if (isEmpty()) {
+			return null;
+		}
+		return data[firstElement];
+	}
+
+	@Override
+	public E dequeue() {
+		if (isEmpty()) {
+			return null;
+		}
+		E answer = data[firstElement];
+		data[firstElement] = null;
+		firstElement = (firstElement + 1) % data.length;
+		size--;
+		return answer;
 	}
 
 }
